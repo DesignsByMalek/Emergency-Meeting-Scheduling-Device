dc0e3668f8d3a8308e3e77157a844f7ba2a6c3db
Karim J <karim.jibai002@gmail.com>
2025-01-06 12:28:48 +0200

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -3,4 +3,9 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
+    '''
+    Runs the server exposed on the local network.
+    Used ngrok when testing over the internet,
+    through a Twilio virtual number/simple Postman request
+    '''
     app.run(debug=True, host="0.0.0.0", port=3000)
